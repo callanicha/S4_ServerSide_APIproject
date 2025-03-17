3626fe61d6447869ac3c9fc547e3d63aacc9deb3
natthanicha <natthanicha.vongjarit@epita.fr>
2025-03-17 14:53:20 +0100

    fix: post method.

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,11 @@ router.get("/", (req, res) => {
     res.send("Users page")
 })
 
+router.post("/", (req, res) => {
+    const userData = req.body;
+    res.status(201).json({ message: "User created", data: userData });
+});
+
 
 module.exports = router;
 
