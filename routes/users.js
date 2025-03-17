1af81188355d8e226990d1e1650f8492f00e4dba
natthanicha <natthanicha.vongjarit@epita.fr>
2025-03-17 12:25:07 +0100

    chores: user page, routes and restful API.

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    res.send("Users page")
+})
+
+
+module.exports = router;
+
+
--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const test = require("./test");
+const router = express.Router();
+const userRoutes = require("../routes/users");
 
 // MIDDLEWARE
 app.use((req, res, next) => {
-    req.calculatedValue = 4 * 7; // Compute 4 * 7
+    test()
+    req.calculatedValue = 4 * 7; 
     console.log("Middleware: Calculated Value =", req.calculatedValue);
     next();
 });
 
+// ROUTES
+app.use("/api/users", userRoutes)
+
+// app.get("/", (req, res) => {
+//     const responseMessage = `Hello, World! The calculated value is ${req.calculatedValue}`;
+//     console.log("Response Sent:", responseMessage);
+//     res.send(responseMessage);
+// });
 
 app.get("/", (req, res) => {
-    const responseMessage = `Hello, World! The calculated value is ${req.calculatedValue}`;
-    console.log("Response Sent:", responseMessage);
-    res.send(responseMessage);
-});
-
+    res.send("Welcome to my API ! e-commerce backed 🤳")
+   })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
