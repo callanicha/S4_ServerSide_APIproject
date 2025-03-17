5beda9cf2f985c15bb3df574cd25fb49e7bab86e
natthanicha <natthanicha.vongjarit@epita.fr>
2025-03-17 11:56:07 +0100

    chores: Added middleware to calculate arithmetic value

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// MIDDLEWARE
+app.use((req, res, next) => {
+    req.calculatedValue = 4 * 7; // Compute 4 * 7
+    console.log("Middleware: Calculated Value =", req.calculatedValue);
+    next();
+});
+
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!")
-    })
+    const responseMessage = `Hello, World! The calculated value is ${req.calculatedValue}`;
+    console.log("Response Sent:", responseMessage);
+    res.send(responseMessage);
+});
+
 
 app.listen(port, () => {
-    console.log(`Example app lostening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
